336fb9815f1149cac6b71164cad39feb34d693ea
chinami1025 <toda.chinami@gmail.com>
2023-01-29 00:07:05 +0900

    サイドバーの実装

---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { SidebarDate } from "./SidebarDate";
+import SidebarIcon from "./SidebarIcon";
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+      <SidebarIcon />
+      <ul className="SidebarList">
+        {SidebarDate.map((value, key) => {
+          return (
+            <li
+              key={key}
+              id={window.location.pathname == value.link ? "active" : ""}
+              className="row"
+              onClick={() => {
+                window.location.pathname = value.link;
+              }}
+            >
+              <div id="icon">{value.icon}</div>
+              <div id="title">{value.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,13 @@
+import Icon from "../images/icon.jpg";
+import React from "react";
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} alt="" />
+      <p>programming@gmail.com</p>
+    </div>
+  );
+}
+
+export default SidebarIcon;
